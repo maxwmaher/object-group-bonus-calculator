3715014b79c0df294ba00e0e09911a1cd7275086
Max Maher <maxwmaher@gmail.com>
2019-07-23 22:04:13 -0500

    used built-in Number() function

--- client.js
+++ client.js
@@ -2,31 +2,31 @@ const employees = [
   {
     name: 'Atticus',
     employeeNumber: '2405',
-    annualSalary: 47000,
+    annualSalary: '47000',
     reviewRating: 3
   },
   {
     name: 'Jem',
     employeeNumber: '62347',
-    annualSalary: 63500,
+    annualSalary: '63500',
     reviewRating: 4
   },
   {
     name: 'Scout',
     employeeNumber: '6243',
-    annualSalary: 74750,
+    annualSalary: '74750',
     reviewRating: 5
   },
   {
     name: 'Robert',
     employeeNumber: '26835',
-    annualSalary: 66000,
+    annualSalary: '66000',
     reviewRating: 1
   },
   {
     name: 'Mayella',
     employeeNumber: '89068',
-    annualSalary: 35000,
+    annualSalary: '35000',
     reviewRating: 1
   }
 ];
@@ -101,7 +101,7 @@ function determineBonus(emp) {
 
   let newBonus = emp.annualSalary * bonusPercentage;
 
-  let newCompensation = emp.annualSalary + newBonus;
+  let newCompensation = Number(emp.annualSalary) + newBonus;
 
   let newEmployeeInfo = {
     name: emp.name,
